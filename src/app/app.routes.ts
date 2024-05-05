4f9db872f50ccb1087ebcfb8a8fb6db930f75656
Chylton M.R <chylton.rezende@hotmail.com>
2024-05-05 20:41:55 -0300

    Ajustado rotas
    Ajustado configuracao de rotas, e ate adicionamos umas rotas no arquivo de rotas para realizar testes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HeaderComponent,
+    },
+    {
+        path: 'teste/componente',
+        component: HeaderComponent,
+    }
+];
