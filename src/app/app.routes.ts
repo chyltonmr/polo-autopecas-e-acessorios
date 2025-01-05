ef475154f4f9e4b1700e8bf62b579702d4f34e20
Chylton M.R <chylton.rezende@hotmail.com>
2025-01-05 15:08:32 -0300

    Inicio projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component'
-import { HeaderComponent } from './components/header/header.component'
 
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: HeaderComponent,
-    },
-    {
-        path: 'teste/componente',
-        component: HeaderComponent,
-    }
-];
+export const routes: Routes = [];
